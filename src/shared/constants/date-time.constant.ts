62bd3f41c6e15fc90a86ac5a3bc7f7b45933bad4
Jinsan T J <hellojinsantj@gmail.com>
2024-08-10 22:39:28 +0530

    added env constant and date time constant

---
+++ src/shared/constants/date-time.constant.ts
@@ -0,0 +1,10 @@
+/**
+ * The application supported date time formats.
+ */
+export const DateTimeFormat = {
+    /**
+     * Eg: 2024-02-26
+     * This date-time format containing only year month & day.
+     */
+    YYY_MM_DD: 'YYYY-MM-DD',
+};
---
+++ src/shared/constants/env.constant.ts
@@ -0,0 +1,14 @@
+/**
+ * The Application supported environments.
+ */
+export const AppEnv = {
+    /**
+     * The development environment.
+     */
+    DEV: 'development',
+
+    /**
+     * The production environment.
+     */
+    PROD: 'production',
+};
